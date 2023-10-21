5d03b91668acb57209b97ebaa16c44aaf96f66e7
yevheniy73 <kaznovsk@ualberta.ca>
2023-10-20 19:15:06 -0600

    Templates, CSS, fontawesome setup

--- socialsink/socialsink/settings.py
+++ socialsink/socialsink/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Add new application
+    'main',
+    'fontawesomefree'
 ]
 
 MIDDLEWARE = [
