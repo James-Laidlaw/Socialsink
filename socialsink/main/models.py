be114775feaedfe7d5ea5b38ab5981d316756727
James-laidlaw <78901538+James-Laidlaw@users.noreply.github.com>
2023-10-21 15:04:37 -0600

    Added likes, comments, tracking follow dismissals

--- socialsink/main/models.py
+++ socialsink/main/models.py
@@ -13,6 +13,7 @@ class Author(models.Model):
 class Follow(models.Model):
     follower = models.ForeignKey(Author, related_name='following', on_delete=models.CASCADE)
     followee = models.ForeignKey(Author, related_name='followed_by', on_delete=models.CASCADE)
+    dismissed = models.BooleanField(default=False) # false if followee has not yet viewed and dismissed the follow request, allows for "Friend Requests"
     created_at = models.DateTimeField(auto_now_add=True)
 
 class Post(models.Model):
@@ -22,3 +23,13 @@ class Post(models.Model):
     publicity = models.IntegerField(default=0) # 0 = private, 1 = friends, 2 = public
     private_to = models.ForeignKey(Author, related_name='readable_by', null=True, on_delete=models.SET_NULL) # only used if publicity = 0
 
+class Comment(models.Model):
+    author = models.ForeignKey(Author, related_name='comments', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    content = models.CharField(max_length=600)
+    timestamp = models.DateTimeField('date published')
+
+class Like(models.Model): 
+    author = models.ForeignKey(Author, related_name='likes', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField('date published')
