b7d3a281af956286987446009fb5cb1aacf604c9
Riley Zilka <rileyzilka33@gmail.com>
2023-10-27 15:51:09 -0600

    swagger working

--- socialsink/main/urls.py
+++ socialsink/main/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
 
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
+
 urlpatterns = [
+    path('api_schema', get_schema_view(title='API Schema', description='Guide for the REST API'), name='api_schema'),
+    path('docs/', TemplateView.as_view(template_name='docs.html', extra_context={'schema_url':'api_schema'}), name='swagger-ui'),
     path("login/", views.login, name="login"),
     path("", views.homepage, name="homepage"),
     path("api/login/", views.loginRequest, name="loginRequest"),
