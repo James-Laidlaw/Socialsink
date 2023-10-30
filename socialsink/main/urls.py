121b98c05835a53d8f37006dc9a4ea85a4ff935d
Abdullah Khadeli <akhadeli@ualberta.ca>
2023-10-30 11:13:46 -0600

    added user creation/signup/registration functionality

--- socialsink/main/urls.py
+++ socialsink/main/urls.py
@@ -3,9 +3,11 @@ from . import views
 
 urlpatterns = [
     path("login/", views.login, name="login"),
+    path("register/", views.register, name="register"),
     path("", views.homepage, name="homepage"),
     path("api/login/", views.loginRequest, name="loginRequest"),
     path("api/logout/", views.logoutRequest, name="logoutRequest"),
+    path("api/register/", views.createAccount, name="createAccount"),
     path("api/make-post/", views.makePost, name="makePost"),
     path("api/delete-account/", views.deleteAccount, name="deleteAccount"),
     path("api/delete-post/<int:id>", views.deletePost, name="deletePost"),
