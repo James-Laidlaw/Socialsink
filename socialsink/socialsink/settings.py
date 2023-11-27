1d61c07f8e0a70ba444ab3603d40b85b99503c5b
Jaskirat Singh Saggu <90576274+j-Saggu@users.noreply.github.com>
2023-11-27 13:42:34 -0700

    made change that fixed 403 issue

--- socialsink/socialsink/settings.py
+++ socialsink/socialsink/settings.py
@@ -186,6 +186,8 @@ CORS_ALLOW_PRIVATE_NETWORK = True
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
+
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
